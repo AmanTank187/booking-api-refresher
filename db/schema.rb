7ba7512f7dacf2d6a7bc56d5b89d6e81900e2975
Aman <amantank187@gmail.com>
2025-11-14 18:34:17 +0000

    Add test to validate the presence of a title when creating an event

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2025_11_14_181250) do
+ActiveRecord::Schema[8.0].define(version: 2025_11_14_182956) do
+  create_table "events", force: :cascade do |t|
+    t.string "title"
+    t.datetime "starts_at"
+    t.datetime "ends_at"
+    t.integer "capacity"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
   create_table "users", force: :cascade do |t|
     t.string "email"
     t.datetime "created_at", null: false
--- test/models/event_test.rb
+++ test/models/event_test.rb
@@ -1,7 +1,13 @@
 require "test_helper"
 
 class EventTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "Event does not save without title" do
+    event = Event.new()
+    assert_not event.save
+  end
+
+  test "Event saves with title" do
+    event = Event.new(title: "New Event")
+    assert event.save
+  end
 end
