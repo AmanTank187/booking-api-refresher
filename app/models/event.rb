43935182dd98a428d68cdef4facc4112ec2f4089
Aman <amantank187@gmail.com>
2025-11-14 19:07:19 +0000

    test: :white_check_mark: Add validation to check ends at date is greater than starts at date

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,6 +1,6 @@
 class Event < ApplicationRecord
   validates :title, presence: true
   validates :starts_at, presence: true
-  validates :ends_at, presence: true
+  validates :ends_at, presence: true, comparison: { greater_than: :starts_at }
   validates :capacity, presence: true
 end
--- test/models/event_test.rb
+++ test/models/event_test.rb
@@ -1,28 +1,33 @@
 require "test_helper"
 
 class EventTest < ActiveSupport::TestCase
-  test "Event does not save without title" do
-    event = Event.new(starts_at: Time.now(), ends_at: Time.now(), capacity: 1)
+  test "Event is not valid without title" do
+    event = Event.new(starts_at: Time.now, ends_at: Time.now, capacity: 1)
     assert_not event.save
   end
 
-  test "Event does not save without starts_at date" do
-    event = Event.new(title: "New Event", ends_at: Time.now(), capacity: 1)
+  test "Event is not valid without starts_at date" do
+    event = Event.new(title: "New Event", ends_at: Time.now, capacity: 1)
     assert_not event.save
   end
 
-  test "Event does not save without ends_at date" do
-    event = Event.new(title: "New Event", starts_at: Time.now(), capacity: 1)
+  test "Event is not valid ends_at date" do
+    event = Event.new(title: "New Event", starts_at: Time.now, capacity: 1)
     assert_not event.save
   end
 
-  test "Event does not save without capacity " do
-    event = Event.new(title: "New Event", starts_at: Time.now(), ends_at: Time.now())
+  test "Event is not valid without capacity " do
+    event = Event.new(title: "New Event", starts_at: Time.now, ends_at: Time.now)
     assert_not event.save
   end
 
-  test "Event saves with title starts_at time, ends_at time" do
-    event = Event.new(title: "New Event", starts_at: Time.now(), ends_at: Time.now(), capacity: 1)
+  test "Event is valid with title starts_at time, ends_at time" do
+    event = Event.new(title: "New Event", starts_at: Time.now, ends_at: Time.now + 1.hour, capacity: 1)
     assert event.save
+  end
+
+  test "Event is not valid if ends at date is before the starts at date" do
+    event = Event.new(title: "New Event", starts_at: Time.now, ends_at: Time.now - (1.hour), capacity: 1)
+    assert_not event.save
   end
 end
