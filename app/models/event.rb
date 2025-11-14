bd106826d347f1e52a72b192d6a9d99a730075e6
Aman <amantank187@gmail.com>
2025-11-14 19:02:18 +0000

    test: :white_check_mark: Test the presence of event values title, starts at, ends at and capacity

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,3 +1,6 @@
 class Event < ApplicationRecord
   validates :title, presence: true
+  validates :starts_at, presence: true
+  validates :ends_at, presence: true
+  validates :capacity, presence: true
 end
--- test/models/event_test.rb
+++ test/models/event_test.rb
@@ -2,12 +2,27 @@ require "test_helper"
 
 class EventTest < ActiveSupport::TestCase
   test "Event does not save without title" do
-    event = Event.new()
+    event = Event.new(starts_at: Time.now(), ends_at: Time.now(), capacity: 1)
     assert_not event.save
   end
 
-  test "Event saves with title" do
-    event = Event.new(title: "New Event")
+  test "Event does not save without starts_at date" do
+    event = Event.new(title: "New Event", ends_at: Time.now(), capacity: 1)
+    assert_not event.save
+  end
+
+  test "Event does not save without ends_at date" do
+    event = Event.new(title: "New Event", starts_at: Time.now(), capacity: 1)
+    assert_not event.save
+  end
+
+  test "Event does not save without capacity " do
+    event = Event.new(title: "New Event", starts_at: Time.now(), ends_at: Time.now())
+    assert_not event.save
+  end
+
+  test "Event saves with title starts_at time, ends_at time" do
+    event = Event.new(title: "New Event", starts_at: Time.now(), ends_at: Time.now(), capacity: 1)
     assert event.save
   end
 end
